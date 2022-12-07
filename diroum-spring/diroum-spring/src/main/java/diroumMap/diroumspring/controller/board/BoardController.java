e9baa41f548b4aed89b4a90da3b2e31966a1f1e9
gitujin <syj991114@gmail.com>
2022-12-07 21:45:56 +0900

    게시글 수정, 삭제 기능 개발

--- diroum-spring/diroum-spring/src/main/java/diroumMap/diroumspring/controller/board/BoardController.java
+++ diroum-spring/diroum-spring/src/main/java/diroumMap/diroumspring/controller/board/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -44,16 +45,50 @@ public class BoardController {
     }
 
     @PostMapping("/register")
-    public String register(@Valid @ModelAttribute PostForm postForm, BindingResult bindingResult, @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser) {
+    public String register(@Valid @ModelAttribute PostForm postForm, BindingResult bindingResult, @SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser, RedirectAttributes redirectAttributes) {
 
         if(bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
             return "board/registerForm";
         }
 
-        boardService.register(postForm.getTitle(), postForm.getContent(), loginUser.getId());
+        Long registerId = boardService.register(postForm.getTitle(), postForm.getContent(), loginUser.getId());
+        redirectAttributes.addAttribute("postId", registerId);
 
         return "redirect:/board";
     }
 
+    @GetMapping("/{postId}/edit")
+    public String editForm(@PathVariable Long postId, Model model){
+
+        Board post = boardService.findOne(postId).orElseThrow();
+
+        PostForm postForm = new PostForm();
+        postForm.setTitle(post.getTitle());
+        postForm.setContent(post.getContent());
+
+        model.addAttribute("postForm", postForm);
+        model.addAttribute("postId", postId);
+
+        return "board/editForm";
+    }
+
+    @PostMapping("/{postId}/edit")
+    public String edit(@PathVariable Long postId, @Valid @ModelAttribute PostForm postForm, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "board/editForm";
+        }
+
+        boardService.updateBoard(postId, postForm.getTitle(), postForm.getContent());
+
+        return "redirect:/board/{postId}";
+    }
+
+    @PostMapping("/{postId}/delete")
+    public String delete(@PathVariable Long postId){
+        boardService.deleteById(postId);
+        return "redirect:/board";
+    }
 }
