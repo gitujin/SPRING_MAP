592dc9be7e6713e7861d02db50cf0edd7c101149
gitujin <syj991114@gmail.com>
2023-07-06 01:11:08 +0900

    configuration

--- diroum-spring/diroum-spring/src/main/java/diroumMap/diroumspring/config/MvcConfig.java
+++ diroum-spring/diroum-spring/src/main/java/diroumMap/diroumspring/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package diroumMap.diroumspring.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
